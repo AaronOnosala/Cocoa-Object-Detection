9786d845807cb2b3244d158945210fd4c4cd7540
Aaron On'osala <164625563+AaronOnosala@users.noreply.github.com>
2024-10-11 08:38:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Importing necessary libraries
 import torch  # PyTorch library for tensor computations
-from ultralytics import YOLO  # YOLO library for object detection models
+from ultralytics import YOLOv10  # YOLO library for object detection models
 
 if __name__ == '__main__':
     # Check if the Metal Performance Shaders (MPS) backend is available for PyTorch
@@ -8,7 +8,7 @@ if __name__ == '__main__':
         mps = torch.device("mps")  # Set the device to MPS (Apple's GPU acceleration)
         
         # Load a YOLO model configuration
-        model = YOLO("yolov9l.yaml")  # Build a new YOLO model from scratch using the specified configuration file
+        model = YOLOv10.from_pretrained("jameslahm/yolov10l")  # Build a new YOLO model from scratch using the specified configuration file
 
         # Train the model using the provided configuration file and number of epochs
         results = model.train(data="config.yaml", epochs=100)  # Train the model for 100 epochs
